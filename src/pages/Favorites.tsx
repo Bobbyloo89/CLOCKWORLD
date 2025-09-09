b758dd2780654243d76e3700c6ec9a63d01300d6
“[Mikael <[mikael.lindstedt@live.se]>
2025-09-09 19:24:33 +0200

    Added styling for Home- and Favorites-page in desktop-mode

--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -4,7 +4,6 @@ import { loadUserCities } from "../utils/storage";
 import CityCard from "../components/city/CityCard";
 import { useNavigate } from "react-router-dom";
 
-
 export default function Favorites() {
   const [list, setList] = useState<CityEntry[]>([]);
   const navigate = useNavigate();
@@ -18,18 +17,32 @@ export default function Favorites() {
   if (favorites.length === 0) {
     return (
       <main>
-        <p>No favorites yet.</p>
-        <button type="button" onClick={() => navigate("/add")}>Add City</button>
+        <div className="favorites-wrapper">
+          <p>No favorites yet.</p>
+          <button type="button" onClick={() => navigate("/add")}>
+            Add City
+          </button>
+        </div>
       </main>
     );
   }
 
   return (
     <main>
-      {favorites.map((c) => (
-        <CityCard key={`${c.country}__${c.city}`} data={c} />
-      ))}
-      <button className="add-city-button" type="button" onClick={() => navigate("/add")}>Add new city</button>
+      <div className="favorites-wrapper">
+        {favorites.map((c) => (
+          <CityCard key={`${c.country}__${c.city}`} data={c} />
+        ))}
+      </div>
+      <div className="add-city-button-wrapper">
+        <button
+          className="add-city-button"
+          type="button"
+          onClick={() => navigate("/add")}
+        >
+          Add new city
+        </button>
+      </div>
     </main>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,16 +5,25 @@ import CityCard from "../components/city/CityCard";
 
 export default function Home() {
   const cities = citiesData as SeedCity[];
-  const featured = cities.filter(c => c.featured === true);
+  const featured = cities.filter((c) => c.featured === true);
   const navigate = useNavigate();
 
   return (
     <main>
-      {featured.map((c) => (
-        <CityCard key={`${c.country}__${c.city}`} data={c} />
-      ))}
-
-      <button className="add-city-button" type="button" onClick={() => navigate("/add")}>Add new city</button>
+      <div className="home-wrapper">
+        {featured.map((c) => (
+          <CityCard key={`${c.country}__${c.city}`} data={c} />
+        ))}
+      </div>
+      <div className="add-city-button-wrapper">
+        <button
+          className="add-city-button"
+          type="button"
+          onClick={() => navigate("/add")}
+        >
+          Add new city
+        </button>
+      </div>
     </main>
   );
 }
